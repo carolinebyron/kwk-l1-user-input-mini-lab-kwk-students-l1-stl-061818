da723a1888f60c18e210d6f921dc2dbed18b3db5
carolinebyron <byroncaroline2@gmail.com>
2018-06-19 14:13:59 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,8 +1,13 @@
 # Code your prompts here!
-
+puts "welcome to st_louis what is your name?"
+name=gets
+puts "oh hello #{name} we are gald you are here"
 # First, puts out a string asking where the tourist would like to stay.
-puts "Let's plan your trip! Where would you like to stay?"
-
+puts "Let's plan your trip! Where would you like to tour first"
+place=gets
+puts "I love the #{place}! We will have so much fun!"
+icecream=gets
+puts "I'd love to #{icecream}"
 # Then, set a variable called stay to get the user's input. Make sure to capitilize that input.
 
 stay = gets.chomp.capitalize  #don't forget to Capitilize the input
